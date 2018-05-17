d6ebce86662e0db9a33c38c9fdf5e1516b7be3a4
github username <github email address>
2018-05-16 17:36:17 -0700

    Question displays Correct or Incorrect based on answer choice

--- client/src/components/Question.js
+++ client/src/components/Question.js
@@ -2,13 +2,24 @@ import React from 'react';
 
 const Answers = (props) =>
 <ol>
-  {props.answers.map(answer => <li>{answer}</li>)}
+  {props.answers.map(answer => <li onClick={props.handleAnswerClick}>{answer}</li>)}
 </ol>
 
-const Question = (props) =>
-<div>
-<strong>{props.question.question}</strong>
-<Answers answers={props.answers}/>
-</div>
+class Question extends React.Component {
+  shouldComponentUpdate(nextProps) {
+    debugger;
+    return this.props.question.question !== nextProps.question.question
+  }
+
+  render() {
+    return(
+      <div>
+      <strong>{this.props.question.question}</strong>
+      <Answers answers={this.props.answers} handleAnswerClick={this.props.handleAnswerClick}/>
+      </div>
+    )
+  }
+}
+
 
 export default Question;
--- client/src/containers/TriviaPage.js
+++ client/src/containers/TriviaPage.js
@@ -4,8 +4,19 @@ import {bindActionCreators} from 'redux';
 import Question from '../components/Question'
 import {fetchQuestions} from '../actions/questionActions';
 
-
+const CorrectResponse = (props) => {
+  <div>
+    <div>Correct!</div>
+    <button>Next Question</button>
+  </div>
+}
 class TriviaPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      renderResult: null
+    }
+  }
   componentDidMount(){
     this.props.fetchQuestions();
   }
@@ -14,15 +25,56 @@ shuffleAnswers = (question) => {
   const answers = [question.correct_answer, ...JSON.parse(question.incorrect_answers)]
       for (let i = answers.length - 1; i > 0; i--) {
           const j = Math.floor(Math.random() * (i + 1));
-          [answers[i], answers[j]] = [answers[j], answers[i]]; 
+          [answers[i], answers[j]] = [answers[j], answers[i]];
       }
       return answers
   }
+
+  findAnswer = (answer) => answer === this.props.currentQuestion.correct_answer
+
+  handleAnswerClick = (event) => {
+    if (!this.state.renderResult) {
+      if (this.findAnswer(event.target.innerText)) {
+        event.target.style.fontWeight = "900"
+        this.setState({
+          renderResult: 'correct'
+        })
+      } else {
+        this.setState({
+          renderResult: 'incorrect'
+        })
+      }
+    }
+  }
+  renderResults = () => {
+    switch(this.state.renderResult){
+      case 'correct':
+      debugger;
+      return( <CorrectResponse />
+        )
+      case 'incorrect':
+      return(
+        <div>
+          <div>Incorrect</div>
+          <button>Next Question</button>
+        </div>)
+
+    }
+    // if (boolean) {
+    //   return(
+    //     <div>Correct!</div>
+    //   )
+    // } else {
+    //   return(
+    //     <div>Incorrect</div>
+    //   )
+    // }
+  }
   render() {
-    const currentQuestion = this.props.questions[0]
     return(
       <div>
-        {currentQuestion ? <Question question={currentQuestion} answers={this.shuffleAnswers(currentQuestion)}/> : "Loading"}
+        {this.props.currentQuestion ? <Question question={this.props.currentQuestion} answers={this.shuffleAnswers(this.props.currentQuestion)} handleAnswerClick={this.handleAnswerClick}/> : "Loading"}
+        {this.renderResults()}
       </div>
     )
   }
@@ -30,7 +82,10 @@ shuffleAnswers = (question) => {
 }
 
 function mapStateToProps(state){
-  return {questions: state.questions}
+  return {
+    questions: state.questions,
+    currentQuestion: state.questions[0]
+  }
 }
 const mapDispatchToProps = (dispatch) => {
   return bindActionCreators({
