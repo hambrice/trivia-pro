e16564fcb720a49935ab3d8ee8867b7c39ec32ba
github username <github email address>
2018-06-01 15:08:56 -0700

    formatted splash page, added type, and added game button

--- client/src/components/SplashPage.js
+++ client/src/components/SplashPage.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const SplashPage = () =>
-<div> Welcome to TriviaPro! </div>
+<div className="jumbotron">
+<h1 className="display-3">Welcome to TriviaPro!</h1>
+<p className="lead">A trivia game built with the magic of React.</p>
+<hr className="my-4" />
+<p>TriviaPro uses trivia questions provided by the <a href="https://opentdb.com/">Open Trivia Database</a>.
+TriviaPro begins with 20 questions, choosing questions at random from various question categories.
+To change the amount of questions or pick a specific category of questions, simply click on "Change Settings".</p>
+<p className="lead">
+  <a className="btn btn-primary btn-lg" href="/game" role="button">Lets Get Started!</a>
+</p>
+</div>
 
 
 export default SplashPage
